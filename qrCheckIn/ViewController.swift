fd3755989dbd78dcabe5f054bcba50bd0c256730
tatetate55 <ruteinyoyokk@gmail.com>
2019-12-19 00:27:23 +0900

    first commit

---
+++ qrCheckIn/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  qrCheckIn
+//
+//  Created by KAMAKURAKAZUHIRO on 2019/12/18.
+//  Copyright © 2019 KAMAKURAKAZUHIRO. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate  {
+    
+    @IBOutlet weak var previewView: UIView!
+    // カメラやマイクの入出力を管理するオブジェクトを生成
+    private let session = AVCaptureSession()
+    
+    var faceTracker:FaceTracker? = nil;
+    var rectView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // カメラやマイクのデバイスそのものを管理するオブジェクトを生成（ここではワイドアングルカメラ・ビデオ・背面カメラを指定）
+        let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera],
+                                                                mediaType: .video,
+                                                                position: .front)
+        // ワイドアングルカメラ・ビデオ・背面カメラに該当するデバイスを取得
+        let devices = discoverySession.devices
+        
+        //　該当するデバイスのうち最初に取得したものを利用する
+        if let backCamera = devices.first {
+            do {
+                // QRコードの読み取りに背面カメラの映像を利用するための設定
+                let deviceInput = try AVCaptureDeviceInput(device: backCamera)
+                
+                if self.session.canAddInput(deviceInput) {
+                    self.session.addInput(deviceInput)
+                    
+                    // 背面カメラの映像からQRコードを検出するための設定
+                    let metadataOutput = AVCaptureMetadataOutput()
+                    
+                    if self.session.canAddOutput(metadataOutput) {
+                        self.session.addOutput(metadataOutput)
+                        
+                        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                        metadataOutput.metadataObjectTypes = [.qr]
+                        
+                        // カメラの映像を画面に表示するためのレイヤーを生成
+                        let previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+                        previewLayer.frame = self.view.bounds
+                        previewLayer.videoGravity = .resizeAspectFill
+                        self.previewView.layer.addSublayer(previewLayer)
+                        
+                        // 読み取り開始
+                        self.session.startRunning()
+                    }
+                }
+            } catch {
+                print("Error occured while creating video device input: \(error)")
+            }
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        for metadata in metadataObjects as! [AVMetadataMachineReadableCodeObject] {
+            // QRコードのデータかどうかの確認
+            if metadata.type != .qr { continue }
+            
+            // 顔認識
+            self.rectView.layer.borderWidth = 3 //四角い枠を用意しておく
+            self.view.addSubview(self.rectView)
+            faceTracker = FaceTracker(view: self.previewView, findface:{arr in
+                let rect = arr[0]
+                self.rectView.frame = rect //四角い枠を顔の位置に移動する
+            })
+        }
+    }
+}
